649a55e704dd4266c7868872d01cd68d5532e80b
Bruce17 <mraith@gmail.com>
2017-01-30 16:28:55 +0100

    add travis script to build and deploy demo application to gh-pages branch

---
+++ deploy-ghpages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change to generate docs dir.
+cd ${SOURCE_DIR}
+
+# Init git in subfolder
+git init
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+
+# Copy files from other folders.
+yes | cp -f ../build/elements.vulcanized.html elements.vulcanized.html
+
+# Add new files.
+git add .
+
+# Push everything back to github.
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REF}" ${DEPLOY_BRANCH}:${TARGET_BRANCH} > /dev/null 2>&1
